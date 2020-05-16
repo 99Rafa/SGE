09df1d84ad461c69aaeec4d389eb511453ab85a8
Rafael <rafael.aguirre.ro@gmail.com>
2020-05-15 20:56:18 -0600

    Creacion del modelo de materias

--- SGE/materias/models.py
+++ SGE/materias/models.py
@@ -1,3 +1,25 @@
+"""Modelo de materias"""
+
+# Django
 from django.db import models
 
-# Create your models here.
+# Modelos
+from grupos.models import Grupo
+from alumnos.models import Alumno
+
+
+class Materia(models.Model):
+    """Modelo de materias
+    
+    Relacion muchos a muchos con alumnos
+    Relacion muchos a uno con grupos
+    """
+
+    grupo = models.ForeignKey(Grupo, on_delete=models.CASCADE)
+    alumnos = models.ManyToManyField(Alumno)
+
+    nombre_materia = models.CharField(max_length=30)
+    no_unidades = models.IntegerField()
+    hrs_teoria = models.IntegerField()
+    hrs_practica = models.IntegerField()
+    creditos = models.IntegerField()
