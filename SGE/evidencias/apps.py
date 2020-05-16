ee73cbb37a67590c70cfe707e4505d8c4eef0541
Rafael <rafael.aguirre.ro@gmail.com>
2020-05-15 19:23:01 -0600

    Creacion inicial de las apps

---
+++ SGE/evidencias/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class EvidenciasConfig(AppConfig):
+    name = 'evidencias'
---
+++ SGE/planes_trabajo/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class PlanesTrabajoConfig(AppConfig):
+    name = 'planes_trabajo'
