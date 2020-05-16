feae80a1f294d35564ba5e602496ba8654ea81c4
Rafael <rafael.aguirre.ro@gmail.com>
2020-05-15 19:49:29 -0600

    Creacion del modelo de evidencias

--- SGE/evidencias/models.py
+++ SGE/evidencias/models.py
@@ -1,3 +1,18 @@
+"""Modelo de evidencias"""
+
+# Django
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Evidencia(models.Model):
+    """Modelo de evidencias
+
+    Modelo que se relaciona de muchos a uno con User
+    """
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    archivo = models.FileField()
+
+    creado = models.DateTimeField(auto_now_add=True)
+    modificado = models.DateTimeField(auto_now=True)
