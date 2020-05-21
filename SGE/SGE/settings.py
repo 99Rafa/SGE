b9beaae7b24b61c02c5071bbbf30a3869a3bf2cc
Rafael <rafael.aguirre.ro@gmail.com>
2020-05-21 11:48:56 -0600

    Including local apps

--- SGE/SGE/settings.py
+++ SGE/SGE/settings.py
@@ -37,6 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Local
+    'alumnos',
+    'grupos',
+    'materias',
+    'calificaciones',
+    'evidencias',
+    'planes_trabajo'
 ]
 
 MIDDLEWARE = [
