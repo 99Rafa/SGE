7e3b0c053323c06a760a0b70a47fc5f91f3ffdfb
Rafael <rafael.aguirre.ro@gmail.com>
2020-05-22 22:02:38 -0600

    Creation of subject model

--- SGE/subjects/models.py
+++ SGE/subjects/models.py
@@ -1,3 +1,23 @@
+"""Subject models"""
+
+# Django
 from django.db import models
 
-# Create your models here.
+# Models
+from groups.models import Group
+
+
+class Subject(models.Model):
+    """Subject model"""
+
+    id_group = models.ForeignKey(Group, on_delete=models.DO_NOTHING)
+
+    name = models.CharField(max_length=50)
+    no_units = models.IntegerField()
+    theory_hours = models.IntegerField()
+    practice_hours = models.IntegerField()
+    credit = models.IntegerField(verbose_name='credits')
+
+    def __str__(self):
+        """Returns subject name"""
+        return f'{self.name}'
