3ca71fcd15384693fb589196f25bb623e43f52a5
Rafael <rafael.aguirre.ro@gmail.com>
2020-05-22 21:03:06 -0600

    Creation of group model

--- SGE/groups/models.py
+++ SGE/groups/models.py
@@ -1,3 +1,15 @@
+"""Group models"""
+
+# Django
 from django.db import models
 
-# Create your models here.
+
+class Group(models.Model):
+    """Group model"""
+
+    semester = models.IntegerField()
+    group = models.CharField(max_length=1)
+
+    def __str__(self):
+        """Returns semester and group"""
+        return f'{self.semester}{self.group}'
