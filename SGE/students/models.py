862c8d01e6b83303725412f4f81ed07d864fb6ca
Rafael <rafael.aguirre.ro@gmail.com>
2020-05-22 21:52:26 -0600

    Creation of student model

--- SGE/students/models.py
+++ SGE/students/models.py
@@ -1,3 +1,16 @@
+"""Student models"""
+
+# Django
 from django.db import models
 
-# Create your models here.
+
+class Student(models.Model):
+    """Student model"""
+
+    no_ctrl = models.CharField(max_length=12)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+
+    def __str__(self):
+        """Return control number"""
+        return f'{self.no_ctrl}'
