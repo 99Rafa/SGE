8a0cf61e7a4573b5f481dc0b982f30286b2497e6
Rafael <rafael.aguirre.ro@gmail.com>
2020-05-25 11:42:36 -0600

    Creation of proxy model for user

--- SGE/users/models.py
+++ SGE/users/models.py
@@ -1,3 +1,17 @@
-from django.db import models
+"""User proxy model"""
 
-# Create your models here.
+# Django
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Profile(models.Model):
+    """Profile model
+
+    Proxy model that extends the base data with other information
+    """
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    profile_picture = models.BinaryField()
+    department = models.CharField(max_length=100)
