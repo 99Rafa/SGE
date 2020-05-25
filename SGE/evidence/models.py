6f32c6b276db0e5f37b9e9050984ca3a4ec64f62
Rafael <rafael.aguirre.ro@gmail.com>
2020-05-25 12:15:08 -0600

    Added subject to evidence model

--- SGE/evidence/models.py
+++ SGE/evidence/models.py
@@ -5,10 +5,13 @@ from django.db import models
 
 # Models
 from django.contrib.auth.models import User
+from subjects.models import Subject
 
 
 class Evidence(models.Model):
     """Evidence model"""
 
     user = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    subject = models.ForeignKey(Subject, on_delete=models.DO_NOTHING)
+
     file = models.BinaryField()
