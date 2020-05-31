05f1f247bb45872d7f38d49e833e0c37288b2b18
Rafael <rafael.aguirre.ro@gmail.com>
2020-05-31 14:00:25 -0600

    Created User URLs

---
+++ SGE/users/urls.py
@@ -0,0 +1,9 @@
+"""User URL configuration"""
+
+from django.urls import path, include
+from users import views
+
+urlpatterns = [
+    path('', include('djoser.urls')),
+    path('', include('djoser.urls.authtoken')),
+]
