9b070c0d0c7c2d03c7c239527f1c20101f1eff4e
Rafael <rafael.aguirre.ro@gmail.com>
2020-05-31 14:09:26 -0600

    Added AUTH_USER_MODEL

--- SGE/SGE/settings.py
+++ SGE/SGE/settings.py
@@ -136,11 +136,13 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+AUTH_USER_MODEL = 'users.User'
+
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
     ),
-    'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAuthenticated'
-    ),
+    # 'DEFAULT_PERMISSION_CLASSES': (
+    #     'rest_framework.permissions.IsAuthenticated'
+    # ),
 }
