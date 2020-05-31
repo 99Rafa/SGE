1d1cf81cecc06816fea345fec31e1b445a248f28
Rafael <rafael.aguirre.ro@gmail.com>
2020-05-31 14:01:47 -0600

    Change from one to one field to direct extension

--- SGE/users/models.py
+++ SGE/users/models.py
@@ -2,16 +2,14 @@
 
 # Django
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser
 
 
-class Profile(models.Model):
+class User(AbstractUser):
     """Profile model
 
     Proxy model that extends the base data with other information
     """
-
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
 
     profile_picture = models.BinaryField(null=True)
     department = models.CharField(max_length=100)
