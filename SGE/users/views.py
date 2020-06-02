640fd9225c6e60d8dbaa4827ba06bc5ef75f4e12
Rafael <rafael.aguirre.ro@gmail.com>
2020-06-01 18:28:19 -0600

    Created user detail view

--- SGE/users/views.py
+++ SGE/users/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+"""User views"""
 
-# Create your views here.
+# Api
+from rest_framework.generics import RetrieveAPIView
+from rest_framework.permissions import IsAuthenticated
+
+# Models
+from users.models import User
+
+# Serializers
+from users.serializers import ProfileSerializer
+
+
+class UserDetailView(RetrieveAPIView):
+    """User detail view"""
+
+    serializer_class = ProfileSerializer
+    permission_classes = [IsAuthenticated]
+
+    lookup_field = 'username'
+    lookup_url_kwarg = 'username'
+    queryset = User.objects.all()
