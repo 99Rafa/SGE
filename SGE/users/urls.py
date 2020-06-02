6cb09f90c8af986ce82312287fc2e9529f0a09c9
Rafael <rafael.aguirre.ro@gmail.com>
2020-06-01 18:28:30 -0600

    Added user detail view

--- SGE/users/urls.py
+++ SGE/users/urls.py
@@ -1,9 +1,13 @@
 """User URL configuration"""
 
+# Django
 from django.urls import path, include
+
+# Views
 from users import views
 
 urlpatterns = [
     path('', include('djoser.urls')),
     path('', include('djoser.urls.authtoken')),
+    path('<str:username>/', views.UserDetailView.as_view())
 ]
