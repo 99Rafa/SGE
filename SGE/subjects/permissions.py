6771b1d2ed508093ccd65e3c650a74c2d1f972ec
Rafael <rafael.aguirre.ro@gmail.com>
2020-06-04 15:42:31 -0600

    Created IsOwner permission

---
+++ SGE/subjects/permissions.py
@@ -0,0 +1,13 @@
+"""Subject permissions"""
+
+# Api
+from rest_framework.permissions import BasePermission
+
+
+class IsOwner(BasePermission):
+
+    message = 'You must be the owner of this object.'
+    
+    def has_object_permission(self, request, view, obj):
+        """Check if the user is the owner"""
+        return request.user == obj.user
