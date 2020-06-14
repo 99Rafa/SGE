de4454f97b0976d345bc01843c623526c25f1445
Rafael <rafael.aguirre.ro@gmail.com>
2020-06-14 13:05:14 -0600

    Changed UserDetailView to only retrieve the current user

--- SGE/users/views.py
+++ SGE/users/views.py
@@ -1,7 +1,11 @@
 """User views"""
 
+# Django
+from django.shortcuts import get_object_or_404
+
 # Api
 from rest_framework.generics import RetrieveUpdateAPIView
+from djoser.conf import settings
 
 # Permissions
 from rest_framework.permissions import IsAuthenticated
@@ -16,13 +20,17 @@ from users.serializers import ProfileSerializer
 
 class UserDetailView(RetrieveUpdateAPIView):
     """User detail view"""
+    permission_classes = [IsAuthenticated]
 
     serializer_class = ProfileSerializer
-    permission_classes = [
-        IsAuthenticated,
-        IsOwner
-    ]
-
-    lookup_field = 'username'
-    lookup_url_kwarg = 'username'
     queryset = User.objects.all()
+
+    def get_queryset(self):
+        qs = User.objects.all()
+        logged_in_user = qs.filter(id=self.request.user.id)
+        return logged_in_user
+
+    def get_object(self):
+        queryset = self.get_queryset()
+        obj = get_object_or_404(queryset, id=self.request.user.id)
+        return obj
