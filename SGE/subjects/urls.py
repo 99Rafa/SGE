304565f68ade699bc719cb50dea18a29e2f1cbb3
Rafael <rafael.aguirre.ro@gmail.com>
2020-06-14 11:12:45 -0600

    Created update_score_view

--- SGE/subjects/urls.py
+++ SGE/subjects/urls.py
@@ -9,4 +9,5 @@ from subjects import views
 urlpatterns = [
     path('get_subjects/', views.get_subjects_view),
     path('<pk>/', views.ListGradesView.as_view()),
+    path('<pk>/update_scores/', views.update_score_view),
 ]
