4493953689e479c456642b922eff232e48094aa4
Rafael <rafael.aguirre.ro@gmail.com>
2020-06-19 19:10:04 -0600

    Created list and detail views for workplan

--- SGE/work_plans/views.py
+++ SGE/work_plans/views.py
@@ -1,3 +1,46 @@
-from django.shortcuts import render
+"""Work plan views"""
 
-# Create your views here.
+# Api 
+from rest_framework.generics import (
+    CreateAPIView,
+    RetrieveUpdateDestroyAPIView,
+    ListAPIView
+)
+
+# Permissions
+from rest_framework.permissions import IsAuthenticated
+from work_plans.permissions import IsOwnerOfSubject, IsOwnerOfWorkplan
+from subjects import permissions as subjectPermissions
+
+# Models
+from work_plans.models import Workplan, RequiredVisit
+from subjects.models import Subject
+
+# Serializers
+from work_plans.serializers import (
+    DetailWorkPlanSerializer,
+    ListWorkPlanSerializer
+)
+
+
+class ListWorkPlanView(ListAPIView):
+
+    serializer_class = ListWorkPlanSerializer
+    permission_classes = [
+        IsAuthenticated,
+        IsOwnerOfSubject
+    ]
+
+    def get_queryset(self):
+        subject_id = self.kwargs['pk']
+        return Workplan.objects.filter(subject=subject_id)
+
+
+class DetailWorkPlanView(RetrieveUpdateDestroyAPIView):
+    
+    serializer_class = DetailWorkPlanSerializer
+    permission_classes = [
+        IsAuthenticated,
+        IsOwnerOfWorkplan
+    ]
+    queryset = Workplan.objects.all()
