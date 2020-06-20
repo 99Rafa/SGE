825e67f9d7c81c195387b8872865f600363095ae
Rafael <rafael.aguirre.ro@gmail.com>
2020-06-19 20:42:35 -0600

    Added create url

--- SGE/work_plans/urls.py
+++ SGE/work_plans/urls.py
@@ -7,6 +7,7 @@ from django.urls import path
 from work_plans import views
 
 urlpatterns = [
+    path('create/', views.CreateWorkplanView.as_view()),
     path('list/<pk>/', views.ListWorkPlanView.as_view()),
     path('detail/<pk>/', views.DetailWorkPlanView.as_view()),
 ]
