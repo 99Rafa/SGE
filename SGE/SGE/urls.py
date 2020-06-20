55a564edbed89ce8641a51458651b12533c9ee84
Rafael <rafael.aguirre.ro@gmail.com>
2020-06-19 19:06:36 -0600

    Added workplan url to urlpatterns

--- SGE/SGE/urls.py
+++ SGE/SGE/urls.py
@@ -6,5 +6,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('user/', include('users.urls')),
-    path('subject/', include('subjects.urls'))
+    path('subject/', include('subjects.urls')),
+    path('workplan/', include('work_plans.urls')),
 ]
