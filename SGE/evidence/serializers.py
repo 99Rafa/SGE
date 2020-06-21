364c4bc8e8d033f2e5d377eaa870d9cf721af98c
Rafael <rafael.aguirre.ro@gmail.com>
2020-06-21 12:29:33 -0600

    Created evidence serializer

---
+++ SGE/evidence/serializers.py
@@ -0,0 +1,14 @@
+"""Evidence serializers"""
+
+# Api
+from rest_framework import serializers
+
+# Models
+from evidence.models import Evidence
+
+
+class EvidenceSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Evidence
+        fields = ['subject', 'title', 'file']
