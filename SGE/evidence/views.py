c878db68c064a6054b421ee9c68498a76718fff1
Rafael <rafael.aguirre.ro@gmail.com>
2020-06-21 15:29:12 -0600

    Separated list and create views

--- SGE/evidence/views.py
+++ SGE/evidence/views.py
@@ -1,9 +1,14 @@
 """Evicence views"""
 
 # Api
-from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.generics import (
+    CreateAPIView,
+    ListAPIView,
+    RetrieveUpdateDestroyAPIView
+)
 # Permissions
 from rest_framework.permissions import IsAuthenticated
+from evidence.permissions import IsOwnerOfSubject, IsOwnerOfEvidence
 
 # Modoels
 from evidence.models import Evidence
@@ -12,7 +17,7 @@ from evidence.models import Evidence
 from evidence.serializers import EvidenceSerializer
 
 
-class EvidenceView(ListCreateAPIView):
+class CreateEvidenceView(CreateAPIView):
 
     serializer_class = EvidenceSerializer
     permission_classes = [IsAuthenticated]
@@ -22,8 +27,25 @@ class EvidenceView(ListCreateAPIView):
         serializer.save(user=self.request.user)
 
 
+class ListEvidenceView(ListAPIView):
+
+    serializer_class = EvidenceSerializer
+    permission_classes = [
+        IsAuthenticated,
+        IsOwnerOfSubject
+    ]
+    queryset = Evidence.objects.all()
+
+    def get_queryset(self):
+        evidence_id = self.kwargs['pk']
+        return Evidence.objects.filter(subject=evidence_id)
+
+
 class DetailEvideceView(RetrieveUpdateDestroyAPIView):
 
     serializer_class = EvidenceSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [
+        IsAuthenticated,
+        IsOwnerOfEvidence
+    ]
     queryset = Evidence.objects.all()
