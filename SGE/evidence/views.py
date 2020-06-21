f813d646a34ce8c4f55a588df2f826e8720c3612
Rafael <rafael.aguirre.ro@gmail.com>
2020-06-21 12:30:35 -0600

    Created evidence views

--- SGE/evidence/views.py
+++ SGE/evidence/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+"""Evicence views"""
 
-# Create your views here.
+# Api
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+# Permissions
+from rest_framework.permissions import IsAuthenticated
+
+# Modoels
+from evidence.models import Evidence
+
+# Serializers
+from evidence.serializers import EvidenceSerializer
+
+
+class EvidenceView(ListCreateAPIView):
+
+    serializer_class = EvidenceSerializer
+    permission_classes = [IsAuthenticated]
+    queryset = Evidence.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+
+class DetailEvideceView(RetrieveUpdateDestroyAPIView):
+
+    serializer_class = EvidenceSerializer
+    permission_classes = [IsAuthenticated]
+    queryset = Evidence.objects.all()
